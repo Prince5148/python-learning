e1b3890c7688854c1ef9e69cf004c2563d8f78d8
Prince5148 <techprince8188@gmail.com>
2025-06-22 10:48:29 +0530

    Added Lecture 1 files – variables and operators
    Uploaded two Python practice files: variables_and_datatypes.py and operators_and_conversion.py. Covered basic variable declaration, type checking, and arithmetic operations.

---
+++ operators_and_conversion.py
@@ -0,0 +1,17 @@
+# Operators and Type Conversion
+
+x = "10"
+y = 5
+
+# Type conversion
+x = int(x)
+
+# Arithmetic Operators
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+
+# Comparison Operators
+print("Is x greater than y?", x > y)
+print("Is x equal to y?", x == y)
