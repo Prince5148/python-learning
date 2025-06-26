addf86ecb16ad84ff1581198f1515912f85cde25
Prince5148 <techprince8188@gmail.com>
2025-06-26 18:06:10 +0530

    Add list_methods_examples.py

---
+++ 03_string_and_tuples/list_methods_examples.py
@@ -0,0 +1,24 @@
+#append method
+list=["prince","tech","AI","software"]
+list.append("technology")
+print(list)                               # output ['prince', 'tech', 'AI', 'software', 'technology']
+
+#insert method
+list=["bike","car","truck"]
+list.insert(0,"bus")
+print(list)                                #output ['bus', 'bike', 'car', 'truck']
+
+#remove method
+list=["leptop","computer","tv"]
+list.remove("tv")
+print(list)                                #output ['leptop', 'computer']
+
+#pop method
+list=["watch","phone","ipad"]
+list.pop()
+print(list)                                #output ['watch', 'phone']
+
+#sort method
+list=[5,4,3,2,1]
+list.sort()
+print(list)                                #output [1, 2, 3, 4, 5]  
