50d738d376dd5d085d6e636d031489c9eba4f42f
Prince5148 <techprince8188@gmail.com>
2025-06-26 18:02:35 +0530

    Add tuple_basics.py

---
+++ 03_string_and_tuples/list_basics.py
@@ -0,0 +1,18 @@
+#Creating lists
+list=["information technology","computer engineering","civil engineering"]
+print("list of branch: ",list)        #output list of branch:  ['information technology', 'computer engineering', 'civil engineering']
+
+#accessing elements
+list=["apple","banana","mango"]
+print(list[0])                        #output apple
+print(list[1])                        #output banana
+list[2]="pineapple"  
+print(list)                           #output ['apple', 'banana', 'pineapple'] list is mutable              
+
+#Slicing
+list=["BMW","MAHINDRA","HYUNDAI","PORSCHE","FERRARY"]
+print(list[0:2])                      #output ['BMW', 'MAHINDRA']
+print(list[1:3])                      #output ['MAHINDRA', 'HYUNDAI']
+
+
+
